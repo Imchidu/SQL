87bceb7ee94150fd5941b3e8a49299fc49299cca
unknown <chidanandxworkz@gmail.com>
2022-12-08 22:59:43 +0530

    sql

---
+++ BANGLORE.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE BANGLORE;
+USE BANGLORE;
+CREATE TABLE AREAS(SL_NO INT,NAME varchar(10),LANDMARK varchar(10),PIN_CODE INT,TYPE varchar(12));
+ insert into AREAS VALUES (1,'btm','xworkz',524255,'residential');
+  insert into AREAS VALUES (2,'jayanagar','WATERTANK',25777,'COMMERCIAL');
+   insert into AREAS VALUES (3,'jpnagar','WELL',5242512,'INDUSTRIAL');
+    insert into AREAS VALUES (4,'rrnagar','TEMPLE',
+    2115,'residential');
+     insert into AREAS VALUES (5,'rajajinagr','GROUND',22154154,'POLITICAL');
+      insert into AREAS VALUES (6,'vijaynagar','MILK SHOP',2588,'residential');
+       insert into AREAS VALUES (7,'indranagr','PG',2588,'residential');
+        insert into AREAS VALUES (8,'gandhinagar','JAIDEVA HOSPITAL',524202,'residential');
+         insert into AREAS VALUES (9,'shantinagar','KALYANI',401165,'residential');
+          insert into AREAS VALUES (10,'banashankari','xworkz',25222,'residential');
+          SELECT * FROM AREAS;
+        
